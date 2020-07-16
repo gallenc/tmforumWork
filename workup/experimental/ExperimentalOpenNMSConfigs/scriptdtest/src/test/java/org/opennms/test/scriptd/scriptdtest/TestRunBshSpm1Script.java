8c4490d0c0b2c15472dae16ca49f68f6da3c8a33
Craig Gallen <cgallen@opennms.org>
2020-07-16 15:27:11 +0100

    working script outside of opennms

--- workup/experimental/ExperimentalOpenNMSConfigs/scriptdtest/src/test/java/org/opennms/test/scriptd/scriptdtest/TestRunBshSpm1Script.java
+++ workup/experimental/ExperimentalOpenNMSConfigs/scriptdtest/src/test/java/org/opennms/test/scriptd/scriptdtest/TestRunBshSpm1Script.java
@@ -13,8 +13,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.opennms.netmgt.xml.event.AlarmData;
 import org.opennms.netmgt.xml.event.Event;
+import org.opennms.netmgt.xml.event.Parm;
+import org.opennms.netmgt.xml.event.Value;
 
 //https://javasourcequery.com/example/org.apache.bsf.lookupBean
 public class TestRunBshSpm1Script {
@@ -58,13 +60,22 @@ public class TestRunBshSpm1Script {
     }
 
     @Test
-    public void test() throws BSFException {
-        log.debug("start of onevent test script");
+    public void testEventHasNoHref() throws BSFException {
+        log.debug("start of testEventHasNoHref test script");
 
         Event event = new Event();
         event.setUei("uei.opennms.org/internal/alarms/AlarmRaised");
         event.setHost("127.0.0.1");
         event.setCreationTime(new Date());
+
+        event.setDbid(10);
+        event.setDescr("this description ");
+        event.setUei("ueistring");
+
+        AlarmData alarmData = new AlarmData();
+        alarmData.setReductionKey("reductionkey");
+        alarmData.setAlarmType(0);
+        event.setAlarmData(alarmData);
 
         mgr.registerBean("event", event);
 
@@ -74,6 +85,51 @@ public class TestRunBshSpm1Script {
 
         mgr.exec("beanshell", "", 0, 0, onEventScript);
 
-        log.debug("end of onevent test script");
+        log.debug("end of testEventHasNoHref test script");
+    }
+    
+    @Test
+    public void testEventHasHref() throws BSFException {
+        log.debug("start of testEventHasHref test script");
+
+        Event event = new Event();
+        event.setUei("uei.opennms.org/internal/alarms/AlarmRaised");
+        event.setHost("127.0.0.1");
+        event.setCreationTime(new Date());
+
+        event.setDbid(10);
+        event.setDescr("this description ");
+        event.setUei("ueistring");
+
+        AlarmData alarmData = new AlarmData();
+        alarmData.setReductionKey("reductionkey");
+        alarmData.setAlarmType(0);
+        event.setAlarmData(alarmData);
+        
+        /* this will add the service problem id to the alarm */
+        Parm parm1 = new Parm();
+        parm1.setParmName("spmID");
+        Value value1 = new Value();
+        value1.setContent("1");
+        parm1.setValue(value1);
+        event.addParm(parm1);
+
+        /* this will add the service problem href to the alarm */
+        Parm parm2 = new Parm();
+        parm2.setParmName("spmHREF");
+        Value value2 = new Value();
+        value2.setContent("href");
+        parm2.setValue(value2);
+        event.addParm(parm2);
+
+        mgr.registerBean("event", event);
+
+        String onEventScript = "     log = bsf.lookupBean(\"log\"); \n" 
+                + "   log.debug(\"running onEvent script\"); \n"
+                + "   source(\"src/main/resources/scriptd-event-spm1.bsh\"); \n";
+
+        mgr.exec("beanshell", "", 0, 0, onEventScript);
+
+        log.debug("end of testEventHasHref test script");
     }
 }
